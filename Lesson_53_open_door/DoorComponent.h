a347de24164cb63b9ed472b41d7fb564c9fcf8a2
phil <phil.vorobyov@gmail.com>
2023-07-07 16:12:04 +0200

    add cpp code to open doors

---
+++ Lesson_53_open_door/DoorComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "DoorComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EDoorState : uint8
+{
+    Closed,
+    Opened,
+    Opening,
+    Closing
+};
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class CPP_PROJECT_API UDoorComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UDoorComponent();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void OpenDoor();
+
+    UFUNCTION()
+    void OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+    UPROPERTY(EditAnywhere, Category = "Door")
+    ATriggerVolume* TriggerVolume;
+
+private:
+    FRotator InitialRotation;
+    FRotator TargetRotation;
+
+    UPROPERTY(EditAnywhere, Category = "Door")
+    float OpenSpeed = 0.2f;
+
+    float OpenAlpha = 0.0f;
+    EDoorState CurrentDoorState = EDoorState::Closed;
+};
